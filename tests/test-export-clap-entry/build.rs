718990fc80a8961a77760dd5d64cdb4f40892930
Marek Miller <mlm@math.ku.dk>
2025-01-31 13:35:48 +0100

    Remove test crate: test-export-clap-entry
     Reasons:
    
     * too fragile
     * won't compile like this on Windows

--- tests/test-export-clap-entry/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    println!("cargo:rustc-link-lib=test_dummy_plugin");
-    println!(
-        "cargo:rustc-link-search=native=target/{}",
-        std::env::var("PROFILE").unwrap()
-    );
-}
