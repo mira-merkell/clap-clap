0e435ee4ad8f34debb22165ab8753c62d009b319
Marek Miller <mlm@math.ku.dk>
2025-02-05 11:46:12 +0100

    Merge tests: entry.rs, entry_bundle.rs

--- tests/entry.rs
+++ tests/entry.rs
@@ -1,4 +1,7 @@
-use std::{ffi::CStr, ptr::null};
+use std::{
+    ffi::{CStr, c_char, c_void},
+    ptr::{null, null_mut},
+};
 
 use clap_clap::{
     entry::{CLAP_PLUGIN_FACTORY_ID, clap_plugin_factory},
@@ -32,6 +35,37 @@ impl_dummy_plugin!(Dummier, "dummier");
 
 clap_clap::entry!(Dummy, Dummier);
 use _clap_entry::clap_entry;
+use clap_sys::{CLAP_VERSION, clap_host};
+
+struct DummyHost(clap_host);
+
+impl DummyHost {
+    fn new() -> Self {
+        extern "C" fn get_extension(_: *const clap_host, _: *const c_char) -> *const c_void {
+            null()
+        }
+        extern "C" fn request_restart(_: *const clap_host) {}
+        extern "C" fn request_process(_: *const clap_host) {}
+        extern "C" fn request_callback(_: *const clap_host) {}
+
+        Self(clap_host {
+            clap_version: CLAP_VERSION,
+            host_data: null_mut(),
+            name: c"".as_ptr(),
+            vendor: c"".as_ptr(),
+            url: c"".as_ptr(),
+            version: c"".as_ptr(),
+            get_extension: Some(get_extension),
+            request_restart: Some(request_restart),
+            request_process: Some(request_process),
+            request_callback: Some(request_callback),
+        })
+    }
+
+    fn as_clap_host(&self) -> &clap_host {
+        &self.0
+    }
+}
 
 #[test]
 fn export_clap_entry() {
@@ -63,6 +97,22 @@ fn export_clap_entry() {
     assert!(!desc.is_null());
     let id = unsafe { CStr::from_ptr((*desc).id) };
     assert_eq!(id, c"dummier");
+
+    let create_plugin = unsafe { *factory }.create_plugin.unwrap();
+    let host = DummyHost::new();
+    let plug = unsafe { create_plugin(factory, host.as_clap_host(), id.as_ptr()) };
+    assert!(!plug.is_null());
+
+    let plug_desc = unsafe { *plug }.desc;
+    assert!(!plug_desc.is_null());
+    let plug_id = unsafe { CStr::from_ptr((*plug_desc).id) };
+    assert_eq!(id, plug_id);
+
+    let plugin_destroy = unsafe { *plug }.destroy.unwrap();
+    unsafe { plugin_destroy(plug) };
+
+    let entry_deinit = clap_entry.deinit.unwrap();
+    unsafe { entry_deinit() }
 
     let entry_deinit = clap_entry.deinit.unwrap();
     unsafe { entry_deinit() }
