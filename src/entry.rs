144b53175dd96328c047ec29618c0b12d50eae5b
⧉⧉⧉ <marek.miller@protonmail.com>
2025-02-06 01:36:50 +0100

    Use LazyLock in entry

--- src/entry.rs
+++ src/entry.rs
@@ -11,39 +11,37 @@ macro_rules! entry {
             use $crate::factory::{Factory, FactoryHost, FactoryPluginDescriptor};
             use $crate::plugin::Plugin;
 
-            pub fn plugin_prototype<P: Plugin>() -> Box<FactoryPluginDescriptor<P>> {
+            use super::*; // Access the types supplied as macro arguments.
+
+            fn plugin_prototype<P: Plugin>() -> Box<FactoryPluginDescriptor<P>> {
                 Box::new(FactoryPluginDescriptor::build_plugin_descriptor()
                             .expect("cannot build factory plugin descriptor"))
             }
 
-            use super::*; // Access the types supplied as macro arguments.
-
-            static FACTORY: std::sync::OnceLock<Factory> = std::sync::OnceLock::new();
-
-            fn factory_init_once<'a>() -> &'a Factory {
-                FACTORY.get_or_init(|| Factory::new(vec![$(plugin_prototype::<$plug>(),)*]))
-            }
+            static FACTORY: std::sync::LazyLock<Factory> =
+                std::sync::LazyLock::new(||
+                    Factory::new(vec![$(plugin_prototype::<$plug>(),)*])
+                );
 
             /// SAFETY: CLAP requires this method to be thread-safe.
-            /// The function factory_init_once() is thread-safe and
+            /// The LazyLock guarding FACTORY is thread-safe and
             /// plugins_count() takes a shared reference to Factory.
             extern "C" fn get_plugin_count(_: *const clap_plugin_factory) -> u32 {
-                factory_init_once().plugins_count()
+                FACTORY.plugins_count()
             }
 
             /// SAFETY: CLAP requires this method to be thread-safe.
-            /// The function factory_init_once() is thread-safe and
+            /// The LazyLock guarding FACTORY is thread-safe and
             /// descriptor() takes a shared reference to Factory.
             extern "C" fn get_plugin_descriptor(
                 _: *const clap_plugin_factory,
                 index: u32,
             ) -> *const clap_plugin_descriptor {
-                factory_init_once().descriptor(index)
-                    .unwrap_or(std::ptr::null())
+                FACTORY.descriptor(index).unwrap_or(std::ptr::null())
             }
 
             /// SAFETY: CLAP requires this method to be thread-safe.
-            /// The function factory_init_once() is thread-safe and
+            /// The LazyLock guarding FACTORY is thread-safe and
             /// boxed_clap_plugin() takes a shared reference to Factory.
             extern "C" fn create_plugin(
                 _: *const clap_plugin_factory,
@@ -58,9 +56,9 @@ macro_rules! entry {
                 // SAFETY: We checked if plug_id is non-null.
                 // The host guarantees that this is a valid C string now.
                 let plugin_id = unsafe { std::ffi::CStr::from_ptr(plugin_id) };
-                factory_init_once()
-                        .create_plugin(plugin_id, host)
-                        .unwrap_or(std::ptr::null_mut())
+                FACTORY
+                    .create_plugin(plugin_id, host)
+                    .unwrap_or(std::ptr::null_mut())
             }
 
             static CLAP_PLUGIN_FACTORY: clap_plugin_factory = clap_plugin_factory {
