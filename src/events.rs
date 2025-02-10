5e29f49d47a5227fd1cce6a49b78191da18b8f26
⧉⧉⧉ <marek.miller@protonmail.com>
2025-02-10 02:21:55 +0100

    Draft a new version of the events.rs module

--- src/events.rs
+++ src/events.rs
@@ -1,6 +1,55 @@
 //! Events and event lists.
 
-use std::fmt::{Display, Formatter};
+use std::{
+    fmt::{Display, Formatter},
+    ptr::slice_from_raw_parts,
+};
+
+use crate::ffi::{CLAP_EVENT_MIDI, clap_event_header, clap_event_midi, clap_input_events};
+
+#[repr(transparent)]
+pub struct Event([u8]);
+
+impl Event {
+    const unsafe fn new(header: &clap_event_header) -> &Self {
+        let len = header.size as usize;
+        let data = &raw const *header as *const _;
+        unsafe { &*(slice_from_raw_parts::<u8>(data, len) as *const _) }
+    }
+
+    const fn as_clap_event_header(&self) -> &clap_event_header {
+        unsafe { &*(self.0.as_ptr() as *const _) }
+    }
+
+    pub const fn midi(&self) -> Result<Midi, Error> {
+        let ev_type = self.as_clap_event_header().r#type;
+
+        if ev_type == CLAP_EVENT_MIDI as u16 {
+            let midi: &clap_event_midi = unsafe { &*(self.0.as_ptr() as *const _) };
+            Ok(Midi::from_clap_event(midi))
+        } else {
+            Err(Error::OtherType(ev_type))
+        }
+    }
+}
+
+pub struct Midi {
+    pub port_index: u16,
+    pub data: [u8; 3],
+}
+
+impl Midi {
+    const fn from_clap_event(event: &clap_event_midi) -> Self {
+        Self {
+            port_index: event.port_index,
+            data: event.data,
+        }
+    }
+}
+
+pub struct InputEvents(*const clap_input_events);
+
+impl InputEvents {}
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum Error {
@@ -8,6 +57,7 @@ pub enum Error {
     UnknownExpression(i32),
     OutOfOrder,
     TryPush,
+    OtherType(u16),
 }
 
 impl Display for Error {
@@ -25,6 +75,8 @@ impl Display for Error {
             Error::TryPush => {
                 write!(f, "event could not be pushed to the queue")
             }
+
+            Error::OtherType(_) => todo!(),
         }
     }
 }
@@ -35,4 +87,26 @@ impl From<Error> for crate::Error {
     fn from(value: Error) -> Self {
         crate::Error::Events(value)
     }
+}
+
+#[test]
+fn cast_midi_event() {
+    let ev = clap_event_midi {
+        header: clap_event_header {
+            size: size_of::<clap_event_midi>() as u32,
+            time: 0,
+            space_id: 0,
+            r#type: CLAP_EVENT_MIDI as u16,
+            flags: 0,
+        },
+        port_index: 7,
+        data: [1, 2, 3],
+    };
+
+    let event = unsafe { Event::new(&ev.header) };
+
+    let midi = event.midi().unwrap();
+
+    assert_eq!(midi.port_index, ev.port_index);
+    assert_eq!(midi.data, ev.data);
 }
