a841c87a8508f946e75aa35da40fc124c263607f
Marek Miller <mlm@math.ku.dk>
2025-02-11 13:39:41 +0100

    Add Midi2 and ParamValue event types

--- src/events.rs
+++ src/events.rs
@@ -1,14 +1,41 @@
 //! Events and event lists.
 
 use std::{
+    ffi::c_void,
     fmt::{Display, Formatter},
-    ptr::slice_from_raw_parts,
+    ptr::{null_mut, slice_from_raw_parts},
 };
 
-use crate::ffi::{
-    CLAP_CORE_EVENT_SPACE_ID, CLAP_EVENT_MIDI, clap_event_header, clap_event_midi,
-    clap_input_events,
+use crate::{
+    ffi::{
+        CLAP_CORE_EVENT_SPACE_ID, CLAP_EVENT_MIDI, CLAP_EVENT_MIDI2, CLAP_EVENT_PARAM_VALUE,
+        clap_event_header, clap_event_midi, clap_event_midi2, clap_event_param_value,
+        clap_input_events,
+    },
+    id::ClapId,
 };
+
+macro_rules! impl_event_cast_methods {
+    ($name:tt, $name_unchk:tt, $type:ty, $cast_type:ty, $clap_id:ident $(,)?) => {
+        /// # Safety
+        ///
+        #[doc = concat!("The caller must ensure that this `Header` has correct size and type to contain the header and the payload of event of the returned type: `", stringify!($name), "`.")]
+        pub const unsafe fn $name_unchk(&self) -> $type {
+            unsafe { <$type>::new_unchecked(self) }
+        }
+
+        pub const fn $name(&self) -> Result<$type, Error> {
+            if self.r#type() != $clap_id as u16 {
+                return Err(Error::OtherType(self.r#type()));
+            }
+            if self.size() != size_of::<$cast_type>() as u32 {
+                return Err(Error::PayloadSize(self.size()));
+            }
+            // SAFETY: We just checked if `self` is a event of type to be cast to.
+            Ok(unsafe { <$type>::new_unchecked(self) })
+        }
+    };
+}
 
 #[derive(Debug, PartialEq)]
 pub struct Header([u8]);
@@ -73,24 +100,21 @@ impl Header {
         self.as_clap_event_header().r#type
     }
 
-    /// # Safety
-    ///
-    /// The caller must ensure that this `Header` has correct size and type to
-    /// contain the header and the payload of event of type: `clap_event_midi`.
-    pub const unsafe fn midi_unchecked(&self) -> Midi {
-        unsafe { Midi::new_unchecked(self) }
-    }
-
-    pub const fn midi(&self) -> Result<Midi, Error> {
-        if self.size() != size_of::<clap_event_midi>() as u32 {
-            return Err(Error::PayloadSize(self.size()));
-        }
-        if self.r#type() != CLAP_EVENT_MIDI as u16 {
-            return Err(Error::OtherType(self.r#type()));
-        }
-        // SAFETY: We just checked if `self` is a event of type: `clap_event_midi`.
-        Ok(unsafe { Midi::new_unchecked(self) })
-    }
+    impl_event_cast_methods!(
+        param_value,
+        param_value_unchecked,
+        ParamValue,
+        clap_event_param_value,
+        CLAP_EVENT_PARAM_VALUE
+    );
+    impl_event_cast_methods!(midi, midi_unchecked, Midi, clap_event_midi, CLAP_EVENT_MIDI);
+    impl_event_cast_methods!(
+        midi2,
+        midi2_unchecked,
+        Midi2,
+        clap_event_midi2,
+        CLAP_EVENT_MIDI2
+    );
 }
 
 pub trait Event {
@@ -113,6 +137,260 @@ pub trait EventBuilder {
     fn event(&self) -> Self::Event<'_>;
 }
 
+macro_rules! impl_event_const_getter {
+    ($name:tt, $cast_method:ident, $type:ty $(,)?) => {
+        pub const fn $name(&self) -> $type {
+            self.$cast_method().$name
+        }
+    };
+}
+
+macro_rules! impl_event_builder_setter {
+    ($name:tt, $type:ty $(,)*) => {
+        pub const fn $name(self, value: $type) -> Self {
+            let mut build = self;
+            build.0.$name = value;
+            build
+        }
+    };
+}
+
+macro_rules! impl_event_builder {
+    ($type:ty, $event_type:ty, $cast_method:ident $(,)?) => {
+        impl EventBuilder for $type {
+            type Event<'a>
+                = $event_type
+            where
+                Self: 'a;
+
+            fn time(self, value: u32) -> Self {
+                let mut build = self;
+                build.0.header.time = value;
+                build
+            }
+
+            fn space_id(self, value: u16) -> Self {
+                let mut build = self;
+                build.0.header.space_id = value;
+                build
+            }
+
+            fn flags(self, value: u32) -> Self {
+                let mut build = self;
+                build.0.header.flags = value;
+                build
+            }
+
+            fn event(&self) -> Self::Event<'_> {
+                // SAFETY: By construction, `self.header` is a valid header of the event type.
+                let header = unsafe { Header::new(&self.0.header) };
+                unsafe { header.$cast_method() }
+            }
+        }
+    };
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct ParamValue<'a> {
+    header: &'a Header,
+}
+
+impl<'a> ParamValue<'a> {
+    /// # Safety
+    ///
+    /// The `header` must be a header of type: `clap_event_param_value`.
+    pub const unsafe fn new_unchecked(header: &'a Header) -> Self {
+        Self { header }
+    }
+
+    const fn as_clap_event_param_value(&self) -> &clap_event_param_value {
+        // SAFETY: By construction, this cast is safe.
+        unsafe { self.header.cast_unchecked() }
+    }
+
+    impl_event_const_getter!(channel, as_clap_event_param_value, i16);
+    impl_event_const_getter!(cookie, as_clap_event_param_value, *mut c_void);
+    impl_event_const_getter!(key, as_clap_event_param_value, i16);
+    impl_event_const_getter!(note_id, as_clap_event_param_value, i32);
+
+    pub fn param_id(&self) -> ClapId {
+        self.as_clap_event_param_value()
+            .param_id
+            .try_into()
+            .unwrap_or(ClapId::invalid_id())
+    }
+
+    impl_event_const_getter!(port_index, as_clap_event_param_value, i16);
+    impl_event_const_getter!(value, as_clap_event_param_value, f64);
+
+    pub const fn build() -> ParamValueBuilder {
+        ParamValueBuilder::new()
+    }
+
+    pub fn update(&self) -> ParamValueBuilder {
+        ParamValueBuilder::with_param_value(self)
+    }
+}
+
+impl Event for ParamValue<'_> {
+    fn header(&self) -> &Header {
+        self.header
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct ParamValueBuilder(clap_event_param_value);
+
+impl ParamValueBuilder {
+    pub const fn new() -> Self {
+        Self(clap_event_param_value {
+            header: clap_event_header {
+                size: size_of::<clap_event_param_value>() as u32,
+                time: 0,
+                space_id: CLAP_CORE_EVENT_SPACE_ID,
+                r#type: CLAP_EVENT_PARAM_VALUE as u16,
+                flags: 0,
+            },
+            param_id: 0,
+            cookie: null_mut(),
+            note_id: 0,
+            port_index: 0,
+            channel: 0,
+            key: 0,
+            value: 0.0,
+        })
+    }
+
+    pub fn with_param_value(param_value: &ParamValue<'_>) -> Self {
+        // SAFETY: ParamValue constructor guarantees that this cast is safe, and we can
+        // copy the object of type: `clap_event_param_value`.
+        Self(*unsafe { param_value.header().cast_unchecked() })
+    }
+
+    impl_event_builder_setter!(port_index, i16);
+    impl_event_builder_setter!(channel, i16);
+    impl_event_builder_setter!(cookie, *mut c_void);
+    impl_event_builder_setter!(key, i16);
+    impl_event_builder_setter!(note_id, i32);
+
+    pub fn param_id(self, value: ClapId) -> Self {
+        let mut builder = self;
+        builder.0.param_id = value.into();
+        builder
+    }
+
+    impl_event_builder_setter!(value, f64);
+}
+
+impl Default for ParamValueBuilder {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl_event_builder!(ParamValueBuilder, ParamValue<'a>, param_value_unchecked);
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct Midi2<'a> {
+    header: &'a Header,
+}
+
+impl<'a> Midi2<'a> {
+    /// # Safety
+    ///
+    /// The `header` must be a header of type: `clap_event_midi2`.
+    pub const unsafe fn new_unchecked(header: &'a Header) -> Self {
+        Self { header }
+    }
+
+    const fn as_clap_event_midi2(&self) -> &clap_event_midi2 {
+        // SAFETY: By construction, this cast is safe.
+        unsafe { self.header.cast_unchecked() }
+    }
+
+    impl_event_const_getter!(port_index, as_clap_event_midi2, u16);
+
+    pub const fn data(&self) -> &[u32; 4] {
+        &self.as_clap_event_midi2().data
+    }
+
+    /// # Example
+    ///
+    /// ```rust
+    /// # use clap_clap::events::{Event, EventBuilder,Midi2};
+    /// let midi = Midi2::build().port_index(1).time(3);
+    /// let event = midi.event();
+    ///
+    /// assert_eq!(event.port_index(), 1);
+    /// assert_eq!(event.header().time(), 3);
+    /// ```
+    pub const fn build() -> Midi2Builder {
+        Midi2Builder::new()
+    }
+
+    /// # Example
+    ///
+    /// ```rust
+    /// # use clap_clap::events::{Event, EventBuilder,Midi2};
+    /// let midi = Midi2::build().port_index(1).data([1, 2, 3, 4]);
+    /// let event = midi.event();
+    ///
+    /// let other_midi = event.update().data([4, 5, 6, 7]);
+    /// let other_event = other_midi.event();
+    ///
+    /// assert_eq!(event.port_index(), 1);
+    /// assert_eq!(event.data(), &[1, 2, 3, 4]);
+    ///
+    /// assert_eq!(other_event.port_index(), 1);
+    /// assert_eq!(other_event.data(), &[4, 5, 6, 7]);
+    /// ```
+    pub fn update(&self) -> Midi2Builder {
+        Midi2Builder::with_midi2(self)
+    }
+}
+
+impl Event for Midi2<'_> {
+    fn header(&self) -> &Header {
+        self.header
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct Midi2Builder(clap_event_midi2);
+
+impl Midi2Builder {
+    pub const fn new() -> Self {
+        Self(clap_event_midi2 {
+            header: clap_event_header {
+                size: size_of::<clap_event_midi2>() as u32,
+                time: 0,
+                space_id: CLAP_CORE_EVENT_SPACE_ID,
+                r#type: CLAP_EVENT_MIDI2 as u16,
+                flags: 0,
+            },
+            port_index: 0,
+            data: [0; 4],
+        })
+    }
+
+    pub fn with_midi2(midi: &Midi2<'_>) -> Self {
+        // SAFETY: Midi constructor guarantees that this cast is safe, and we can copy
+        // the object of type: `clap_event_midi2`.
+        Self(*unsafe { midi.header().cast_unchecked() })
+    }
+
+    impl_event_builder_setter!(port_index, u16);
+    impl_event_builder_setter!(data, [u32; 4]);
+}
+
+impl Default for Midi2Builder {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl_event_builder!(Midi2Builder, Midi2<'a>, midi2_unchecked);
+
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Midi<'a> {
     header: &'a Header,
@@ -131,9 +409,7 @@ impl<'a> Midi<'a> {
         unsafe { self.header.cast_unchecked() }
     }
 
-    pub const fn port_index(&self) -> u16 {
-        self.as_clap_event_midi().port_index
-    }
+    impl_event_const_getter!(port_index, as_clap_event_midi, u16);
 
     pub const fn data(&self) -> &[u8; 3] {
         &self.as_clap_event_midi().data
@@ -204,17 +480,8 @@ impl MidiBuilder {
         Self(*unsafe { midi.header().cast_unchecked() })
     }
 
-    pub const fn port_index(self, value: u16) -> Self {
-        let mut build = self;
-        build.0.port_index = value;
-        build
-    }
-
-    pub const fn data(self, value: [u8; 3]) -> Self {
-        let mut build = self;
-        build.0.data = value;
-        build
-    }
+    impl_event_builder_setter!(port_index, u16);
+    impl_event_builder_setter!(data, [u8; 3]);
 }
 
 impl Default for MidiBuilder {
@@ -223,37 +490,7 @@ impl Default for MidiBuilder {
     }
 }
 
-impl EventBuilder for MidiBuilder {
-    type Event<'a>
-        = Midi<'a>
-    where
-        Self: 'a;
-
-    fn time(self, value: u32) -> Self {
-        let mut build = self;
-        build.0.header.time = value;
-        build
-    }
-
-    fn space_id(self, value: u16) -> Self {
-        let mut build = self;
-        build.0.header.space_id = value;
-        build
-    }
-
-    fn flags(self, value: u32) -> Self {
-        let mut build = self;
-        build.0.header.flags = value;
-        build
-    }
-
-    fn event(&self) -> Self::Event<'_> {
-        // SAFETY: By construction, `self.header` is a valid header of type:
-        // `clap_event_midi`.
-        let header = unsafe { Header::new(&self.0.header) };
-        unsafe { header.midi_unchecked() }
-    }
-}
+impl_event_builder!(MidiBuilder, Midi<'a>, midi_unchecked);
 
 pub struct InputEvents<'a>(&'a clap_input_events);
 
--- tests/unit/events.rs
+++ tests/unit/events.rs
@@ -53,6 +53,109 @@ fn cast_header() {
     assert_eq!(header.r#type(), CLAP_EVENT_MIDI as u16);
 }
 
+mod param_value {
+    use std::ptr::null_mut;
+
+    use clap_clap::{
+        events,
+        events::{EventBuilder, Header, ParamValue},
+        ffi::{
+            CLAP_EVENT_NOTE_CHOKE, CLAP_EVENT_PARAM_VALUE, clap_event_header,
+            clap_event_param_value,
+        },
+    };
+
+    #[test]
+    fn try_01() {
+        let event = clap_event_param_value {
+            header: clap_event_header {
+                size: 33,
+                time: 0,
+                space_id: 0,
+                r#type: CLAP_EVENT_PARAM_VALUE as u16,
+                flags: 0,
+            },
+            param_id: 0,
+            cookie: null_mut(),
+            note_id: 0,
+            port_index: 0,
+            channel: 0,
+            key: 0,
+            value: 0.0,
+        };
+
+        let header = unsafe { Header::new(&event.header) };
+        assert_eq!(Err(events::Error::PayloadSize(33)), header.param_value())
+    }
+
+    #[test]
+    fn try_02() {
+        let event = clap_event_param_value {
+            header: clap_event_header {
+                size: size_of::<clap_event_param_value>() as u32,
+                time: 0,
+                space_id: 0,
+                r#type: CLAP_EVENT_NOTE_CHOKE as u16,
+                flags: 0,
+            },
+            param_id: 0,
+            cookie: null_mut(),
+            note_id: 0,
+            port_index: 0,
+            channel: 0,
+            key: 0,
+            value: 0.0,
+        };
+
+        let header = unsafe { Header::new(&event.header) };
+        assert_eq!(
+            Err(events::Error::OtherType(CLAP_EVENT_NOTE_CHOKE as u16)),
+            header.midi()
+        )
+    }
+
+    #[test]
+    fn try_03() {
+        let event = clap_event_param_value {
+            header: clap_event_header {
+                size: size_of::<clap_event_param_value>() as u32,
+                time: 0,
+                space_id: 0,
+                r#type: CLAP_EVENT_PARAM_VALUE as u16,
+                flags: 0,
+            },
+            param_id: 0,
+            cookie: null_mut(),
+            note_id: 0,
+            port_index: 87,
+            channel: 1,
+            key: 0,
+            value: 123.456,
+        };
+
+        let header = unsafe { Header::new(&event.header) };
+        let _ = header.midi().unwrap_err();
+        let event = header.param_value().unwrap();
+        assert_eq!(event.port_index(), 87);
+        assert_eq!(event.value(), 123.456);
+    }
+
+    #[test]
+    fn build() {
+        let param_value1 = ParamValue::build().port_index(1).value(123.456);
+        let param_value2 = param_value1.port_index(3);
+
+        let event1 = param_value1.event();
+        let event2 = param_value2.event();
+
+        assert_eq!(event1.value(), 123.456);
+        assert_eq!(event2.value(), 123.456);
+
+        assert_eq!(event1.port_index(), 1);
+        assert_eq!(event2.port_index(), 3);
+    }
+}
+
 mod midi {
     use clap_clap::{
         events,
@@ -135,18 +238,100 @@ mod midi {
     }
 }
 
+mod midi2 {
+    use clap_clap::{
+        events,
+        events::{EventBuilder, Header, Midi2},
+        ffi::{CLAP_EVENT_MIDI2, CLAP_EVENT_NOTE_CHOKE, clap_event_header, clap_event_midi2},
+    };
+
+    #[test]
+    fn try_midi2_01() {
+        let event = clap_event_midi2 {
+            header: clap_event_header {
+                size: 3,
+                time: 0,
+                space_id: 0,
+                r#type: CLAP_EVENT_MIDI2 as u16,
+                flags: 0,
+            },
+            port_index: 0,
+            data: [0, 0, 0, 0],
+        };
+
+        let header = unsafe { Header::new(&event.header) };
+        assert_eq!(Err(events::Error::PayloadSize(3)), header.midi2())
+    }
+
+    #[test]
+    fn try_midi2_02() {
+        let event = clap_event_midi2 {
+            header: clap_event_header {
+                size: size_of::<clap_event_midi2>() as u32,
+                time: 0,
+                space_id: 0,
+                r#type: CLAP_EVENT_NOTE_CHOKE as u16,
+                flags: 0,
+            },
+            port_index: 0,
+            data: [0, 0, 0, 0],
+        };
+
+        let header = unsafe { Header::new(&event.header) };
+        assert_eq!(
+            Err(events::Error::OtherType(CLAP_EVENT_NOTE_CHOKE as u16)),
+            header.midi2()
+        )
+    }
+
+    #[test]
+    fn try_midi_03() {
+        let event = clap_event_midi2 {
+            header: clap_event_header {
+                size: size_of::<clap_event_midi2>() as u32,
+                time: 0,
+                space_id: 0,
+                r#type: CLAP_EVENT_MIDI2 as u16,
+                flags: 0,
+            },
+            port_index: 87,
+            data: [1, 2, 3, 4],
+        };
+
+        let header = unsafe { Header::new(&event.header) };
+        let midi = header.midi2().unwrap();
+        assert_eq!(midi.data(), &[1, 2, 3, 4]);
+        assert_eq!(midi.port_index(), 87);
+    }
+
+    #[test]
+    fn build() {
+        let midi1 = Midi2::build().port_index(1).data([5, 6, 7, 8]);
+        let midi2 = midi1.port_index(3);
+
+        let event1 = midi1.event();
+        let event2 = midi2.event();
+
+        assert_eq!(event1.data(), &[5, 6, 7, 8]);
+        assert_eq!(event2.data(), &[5, 6, 7, 8]);
+
+        assert_eq!(event1.port_index(), 1);
+        assert_eq!(event2.port_index(), 3);
+    }
+}
+
 mod input_events {
     use std::ptr::null_mut;
 
     use clap_clap::{
         events::InputEvents,
         ffi::{
-            CLAP_CORE_EVENT_SPACE_ID, CLAP_EVENT_MIDI, clap_event_header, clap_event_midi,
-            clap_input_events,
+            CLAP_CORE_EVENT_SPACE_ID, CLAP_EVENT_MIDI, CLAP_EVENT_PARAM_VALUE, clap_event_header,
+            clap_event_midi, clap_event_param_value, clap_input_events,
         },
     };
 
-    static MIDI_EVENT: clap_event_midi = clap_event_midi {
+    static EVENT1: clap_event_midi = clap_event_midi {
         header: clap_event_header {
             size: size_of::<clap_event_midi>() as u32,
             time: 1,
@@ -158,17 +343,21 @@ mod input_events {
         data: [1, 2, 3],
     };
 
-    // TODO: once other event types are implemented, change this to some other type.
-    static MIDI_EVENT2: clap_event_midi = clap_event_midi {
+    const EVENT2: clap_event_param_value = clap_event_param_value {
         header: clap_event_header {
-            size: size_of::<clap_event_midi>() as u32,
+            size: size_of::<clap_event_param_value>() as u32,
             time: 2,
             space_id: CLAP_CORE_EVENT_SPACE_ID,
-            r#type: CLAP_EVENT_MIDI as u16,
+            r#type: CLAP_EVENT_PARAM_VALUE as u16,
             flags: 0,
         },
+        param_id: 0,
+        cookie: null_mut(),
+        note_id: 0,
         port_index: 0,
-        data: [5, 6, 7],
+        channel: 0,
+        key: 0,
+        value: 123.456,
     };
 
     extern "C-unwind" fn test_size(_: *const clap_input_events) -> u32 {
@@ -180,9 +369,9 @@ mod input_events {
         index: u32,
     ) -> *const clap_event_header {
         if index == 0 {
-            &MIDI_EVENT.header
+            &EVENT1.header
         } else {
-            &MIDI_EVENT2.header
+            &EVENT2.header
         }
     }
 
@@ -198,12 +387,14 @@ mod input_events {
         assert_eq!(input_events.size(), 2);
 
         let header = input_events.get(0);
+        let _ = header.midi2().unwrap_err();
         let event = header.midi().unwrap();
         assert_eq!(event.data(), &[1, 2, 3]);
 
         let header = input_events.get(1);
-        let event = header.midi().unwrap();
-        assert_eq!(event.data(), &[5, 6, 7]);
+        let _ = header.midi().unwrap_err();
+        let event = header.param_value().unwrap();
+        assert_eq!(event.value(), 123.456);
     }
 
     #[should_panic(expected = "index out of bounds")]
