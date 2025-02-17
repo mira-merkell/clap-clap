fb68ab5bf2126c691ecd3dd205f76451ecfd77ae
⧉⧉⧉ <marek.miller@protonmail.com>
2025-02-17 22:30:44 +0100

    Move HostAudioPort to (private) module: host

--- src/ext/audio_ports.rs
+++ src/ext/audio_ports.rs
@@ -2,16 +2,7 @@ use std::fmt::{Display, Formatter};
 
 pub(crate) use ffi::ClapPluginAudioPorts;
 
-use crate::{
-    ffi::{
-        CLAP_AUDIO_PORTS_RESCAN_CHANNEL_COUNT, CLAP_AUDIO_PORTS_RESCAN_FLAGS,
-        CLAP_AUDIO_PORTS_RESCAN_IN_PLACE_PAIR, CLAP_AUDIO_PORTS_RESCAN_LIST,
-        CLAP_AUDIO_PORTS_RESCAN_NAMES, CLAP_AUDIO_PORTS_RESCAN_PORT_TYPE, clap_host_audio_ports,
-    },
-    impl_flags_u32,
-    plugin::Plugin,
-    prelude::Host,
-};
+use crate::plugin::Plugin;
 
 pub trait AudioPorts<P>
 where
@@ -400,71 +391,86 @@ mod static_ports {
         }
     }
 }
+pub use host::{HostAudioPorts, RescanFlags};
 pub use static_ports::{MonoPorts, StereoPorts};
+mod host {
+    use std::fmt::Display;
 
-/// Port rescan flags.
-///
-/// # Example
-///
-/// ```rust
-/// # use clap_clap::ext::audio_ports::RescanFlags;
-/// assert_eq!(RescanFlags::Names as u32, 0b1);
-/// assert!(RescanFlags::Names.is_set(0b101));
-/// assert_eq!(RescanFlags::Names.set(0b100), 0b101);
-/// assert_eq!(RescanFlags::Names.clear(0b101), 0b100);
-/// ```
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-#[repr(u32)]
-pub enum RescanFlags {
-    /// The ports name did change, the host can scan them right away.
-    Names = CLAP_AUDIO_PORTS_RESCAN_NAMES,
-    ///  The flags did change
-    Flags = CLAP_AUDIO_PORTS_RESCAN_FLAGS,
-    ///  The channel_count did change
-    ChannelCount = CLAP_AUDIO_PORTS_RESCAN_CHANNEL_COUNT,
-    ///  The port type did change
-    PortType = CLAP_AUDIO_PORTS_RESCAN_PORT_TYPE,
-    ///  The in-place pair did change
-    InPlacePair = CLAP_AUDIO_PORTS_RESCAN_IN_PLACE_PAIR,
-    ///  The list of ports have changed: entries have been removed/added.
-    List = CLAP_AUDIO_PORTS_RESCAN_LIST,
-}
+    use crate::{
+        ffi::{
+            CLAP_AUDIO_PORTS_RESCAN_CHANNEL_COUNT, CLAP_AUDIO_PORTS_RESCAN_FLAGS,
+            CLAP_AUDIO_PORTS_RESCAN_IN_PLACE_PAIR, CLAP_AUDIO_PORTS_RESCAN_LIST,
+            CLAP_AUDIO_PORTS_RESCAN_NAMES, CLAP_AUDIO_PORTS_RESCAN_PORT_TYPE,
+            clap_host_audio_ports,
+        },
+        host::Host,
+        impl_flags_u32,
+    };
 
-impl_flags_u32!(RescanFlags);
-
-#[derive(Debug)]
-pub struct HostAudioPorts<'a> {
-    host: &'a Host,
-    clap_host_audio_ports: &'a clap_host_audio_ports,
-}
-
-impl<'a> HostAudioPorts<'a> {
-    /// # Safety
+    /// Port rescan flags.
     ///
-    /// All extension interface function pointers must be non-null (Some), and
-    /// the functions must be thread-safe.
-    pub(crate) const unsafe fn new_unchecked(
+    /// # Example
+    ///
+    /// ```rust
+    /// # use clap_clap::ext::audio_ports::RescanFlags;
+    /// assert_eq!(RescanFlags::Names as u32, 0b1);
+    /// assert!(RescanFlags::Names.is_set(0b101));
+    /// assert_eq!(RescanFlags::Names.set(0b100), 0b101);
+    /// assert_eq!(RescanFlags::Names.clear(0b101), 0b100);
+    /// ```
+    #[derive(Debug, Copy, Clone, PartialEq, Eq)]
+    #[repr(u32)]
+    pub enum RescanFlags {
+        /// The ports name did change, the host can scan them right away.
+        Names = CLAP_AUDIO_PORTS_RESCAN_NAMES,
+        ///  The flags did change
+        Flags = CLAP_AUDIO_PORTS_RESCAN_FLAGS,
+        ///  The channel_count did change
+        ChannelCount = CLAP_AUDIO_PORTS_RESCAN_CHANNEL_COUNT,
+        ///  The port type did change
+        PortType = CLAP_AUDIO_PORTS_RESCAN_PORT_TYPE,
+        ///  The in-place pair did change
+        InPlacePair = CLAP_AUDIO_PORTS_RESCAN_IN_PLACE_PAIR,
+        ///  The list of ports have changed: entries have been removed/added.
+        List = CLAP_AUDIO_PORTS_RESCAN_LIST,
+    }
+
+    impl_flags_u32!(RescanFlags);
+
+    #[derive(Debug)]
+    pub struct HostAudioPorts<'a> {
         host: &'a Host,
         clap_host_audio_ports: &'a clap_host_audio_ports,
-    ) -> Self {
-        Self {
-            host,
-            clap_host_audio_ports,
+    }
+
+    impl<'a> HostAudioPorts<'a> {
+        /// # Safety
+        ///
+        /// All extension interface function pointers must be non-null (Some),
+        /// and the functions must be thread-safe.
+        pub(crate) const unsafe fn new_unchecked(
+            host: &'a Host,
+            clap_host_audio_ports: &'a clap_host_audio_ports,
+        ) -> Self {
+            Self {
+                host,
+                clap_host_audio_ports,
+            }
         }
-    }
 
-    pub fn is_rescan_flag_supported(&self, flag: RescanFlags) -> bool {
-        // SAFETY: By construction, the callback must be a valid function pointer,
-        // and the call is thread-safe.
-        let callback = self.clap_host_audio_ports.is_rescan_flag_supported.unwrap();
-        unsafe { callback(self.host.clap_host(), flag as u32) }
-    }
+        pub fn is_rescan_flag_supported(&self, flag: RescanFlags) -> bool {
+            // SAFETY: By construction, the callback must be a valid function pointer,
+            // and the call is thread-safe.
+            let callback = self.clap_host_audio_ports.is_rescan_flag_supported.unwrap();
+            unsafe { callback(self.host.clap_host(), flag as u32) }
+        }
 
-    pub fn rescan(&self, flags: u32) {
-        // SAFETY: By construction, the callback must be a valid function pointer,
-        // and the call is thread-safe.
-        let callback = self.clap_host_audio_ports.rescan.unwrap();
-        unsafe { callback(self.host.clap_host(), flags) };
+        pub fn rescan(&self, flags: u32) {
+            // SAFETY: By construction, the callback must be a valid function pointer,
+            // and the call is thread-safe.
+            let callback = self.clap_host_audio_ports.rescan.unwrap();
+            unsafe { callback(self.host.clap_host(), flags) };
+        }
     }
 }
 
